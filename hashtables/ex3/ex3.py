63bcfd937b363030768dfea37709ec4a338a257f
Afia-Lydia <you@example.com>
2020-08-07 13:32:42 -0400

    completed mvp for ex4 and am passing tests. will work on interview questions and document as much as i can for ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,16 +2,45 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
 
-    return result
+    allnums = {}
+
+    for array in arrays:
+        allnums[array] = array
+        for i in array:
+            if i in allnums.keys():
+                allnums[i] +=1
+            else:
+                allnums[i]=1
+            
+            num = allnums[i]
+
+            # if num > 1:
+            #     print(num)
 
 
-if __name__ == "__main__":
-    arrays = []
+    # print(result)
+    print(allnums)
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+
+
+    
+    #     result = []
+
+    #     if allnums[i] > 1:
+    #         result.append(i)    
+
+    # return result
+
+intersection([[1,2],[1,4,3],[1,6,5]])
+
+# if __name__ == "__main__":
+#     arrays = []
+
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+#     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,20 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = [] # <- created array for answer
+    numbs = {} #<- created a dictionary or hashtable to store my values
+
+    for num in a: 
+        if num < 0: #<- if the number is less than zero, so like any of the negative numbers I'm going to do the following
+            numbs[-num] = num # <-  recording the keys of negative numbers as opposite when storing in our dictionary/hashtable 
+            #this is only adding the neg numbers to the numbs array
+    
+    print("negg",numbs.keys())
+    
+    for num in a: #looping through array from the param again
+        if num in numbs: #<- basically if when looping I come upon  any of the same numbers as keys I'm appending it to the result array
+            print("db",num)
+            result.append(num)
 
     return result
 
